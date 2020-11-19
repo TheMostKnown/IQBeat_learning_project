f7f55b2e48dec692ceea5a6d78336e2ba1b93cd4
alex <allakos01@mail.ru>
2020-11-20 02:06:26 +0300

    now il looks like working

--- Json_opener_web/file_working/forms.py
+++ Json_opener_web/file_working/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class DocumentForm(forms.Form):
+     key = forms.CharField(max_length=200)
      file = forms.FileField()
-     key = forms.CharField()
--- Json_opener_web/file_working/views.py
+++ Json_opener_web/file_working/views.py
@@ -5,12 +5,27 @@ from .forms import DocumentForm
 
 import json
 
+def handle_uploaded_file(f):
+    with open('use_it.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def index(request):
     if request.method == "GET":
         return render(request, 'index.html')
     elif request.method == "POST":
-        form = DocumentForm(request.POST)
+        form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            text = json.load(form['file'])
-            k = form['key']
-            return HttpResponse(text[k])
+            handle_uploaded_file(request.FILES['file'])
+            k = request.POST['key']
+            try:
+                with open('use_it.txt', 'r') as text:
+                    t = json.load(text)
+                    if k == '*':
+                        return HttpResponse(t)
+                    else:
+                       return HttpResponse(t[k])
+            except Exception:
+                return HttpResponse("Something_go_wrong")
+        else: return HttpResponse("Something_go_wrong")
